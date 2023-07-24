5faa6b5533d51948d0f10366a46d1c122bb2a1f2
Blacktrecks <petrica.roman@student.usv.ro>
2023-07-24 11:07:07 +0300

    Implement notes and tasks in user panel using notion on day 11

--- src/app/components/user-template/user-panel/user-panel.component.ts
+++ src/app/components/user-template/user-panel/user-panel.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarToggleService } from 'src/app/services/navbar-toggle.service';
+import { Client } from '@notionhq/client';
+import { NotionService } from 'src/app/services/notion.service';
 
 @Component({
   selector: 'app-user-panel',
   templateUrl: './user-panel.component.html',
   styleUrls: ['./user-panel.component.css']
 })
-export class UserPanelComponent {
-  constructor(private navbarToggleService: NavbarToggleService) {
+export class UserPanelComponent implements OnInit {
+  userTasks: any[] = [];
+  userNotes: any[] = [];
+  showTasks: boolean = false;
+  showNotes: boolean = false;
+
+  constructor(private navbarToggleService: NavbarToggleService, private notionService: NotionService) {
     this.navbarToggleService.setShowNavbar(false);
+  }
+  ngOnInit(): void {
+    // Fetch user tasks from Notion database
+    this.notionService.getUserTasks().then(tasks => {
+      this.userTasks = tasks;
+    });
+
+    // Fetch user notes from Notion database
+    this.notionService.getUserNotes().then(notes => {
+      this.userNotes = notes;
+    });
+  }
+
+  // Show user tasks when the 'User Tasks' tab is clicked
+  showUserTasks() {
+    this.showTasks = true;
+    this.showNotes = false;
+  }
+
+  // Show user notes when the 'User Notes' tab is clicked
+  showUserNotes() {
+    this.showTasks = false;
+    this.showNotes = true;
   }
 }
---
+++ src/app/services/notion.service.ts
@@ -0,0 +1,99 @@
+import { Injectable } from '@angular/core';
+import { Client } from '@notionhq/client';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotionService {
+  private notion: Client;
+
+  constructor() {
+    // Replace 'YOUR_INTEGRATION_SECRET' with your actual Notion Integration Secret (API key)
+    this.notion = new Client({ auth: 'secret_a6uZ1lq5psi794coIxn5VhONUDgZty4UmbM681xjXCP' });
+  }
+
+  // Fetch user tasks from Notion database
+  getUserTasks(): Promise<any[]> {
+    return this.notion.databases.query({
+      database_id: 'f4804b5d97b0436a98b1cdd2d5180aa3?v=89d6a959cc154bee98acd32301ac34d7' // Replace with your actual Notion database ID for user tasks
+    })
+    .then(response => {
+      return response.results;
+    })
+    .catch(error => {
+      console.error('Error fetching tasks:', error);
+      return [];
+    });
+  }
+
+  // Fetch user notes from Notion database
+  getUserNotes(): Promise<any[]> {
+    return this.notion.databases.query({
+      database_id: 'a2ee441aac944e86a588d91f503b9ba1?v=227d4dd55f094cd389b657b80a0b2630' // Replace with your actual Notion database ID for user notes
+    })
+    .then(response => {
+      return response.results;
+    })
+    .catch(error => {
+      console.error('Error fetching notes:', error);
+      return [];
+    });
+  }
+
+  // Add a new note to the user notes database
+  addNewNote(title: string, description: string): Promise<any> {
+    return this.notion.pages.create({
+      parent: {
+        database_id: 'a2ee441aac944e86a588d91f503b9ba1?v=227d4dd55f094cd389b657b80a0b2630' // Replace with your actual Notion database ID for user notes
+      },
+      properties: {
+        title: {
+          title: [
+            {
+              type: 'text',
+              text: {
+                content: title
+              }
+            }
+          ]
+        },
+        description: {
+          rich_text: [
+            {
+              type: 'text',
+              text: {
+                content: description
+              }
+            }
+          ]
+        }
+      }
+    })
+    .then(response => {
+      return response;
+    })
+    .catch(error => {
+      console.error('Error adding note:', error);
+      return null;
+    });
+  }
+
+  // Update a user task in the database
+  updateTask(taskId: string, completed: boolean): Promise<any> {
+    return this.notion.pages.update({
+      page_id: taskId,
+      properties: {
+        completed: {
+          checkbox: completed
+        }
+      }
+    })
+    .then(response => {
+      return response;
+    })
+    .catch(error => {
+      console.error('Error updating task:', error);
+      return null;
+    });
+  }
+}
